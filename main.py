db80445f22020020328ae5da07c5716cea14dfdb
Farid <rahfar777@gmail.com>
2024-09-13 15:17:33 +0300

    Revert "WIP"
    This reverts commit 5db83dc937150a5bb68ccc674075ed1834036286.

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
 from fastapi import FastAPI
+import tensorflow as tf
+import numpy as np
 from memory_profiler import profile
 
 app = FastAPI()
 
+# Load the TensorFlow model
+model = tf.keras.models.load_model("simple_model.keras")
+
+
+def predict(input_data):
+    # Perform prediction using the loaded model
+    prediction = model.predict(input_data)
+    return prediction
 
 @profile
 @app.post("/predict")
 async def predict_handler():
-    return {"prediction": []}
+    # Generate random input data (784 features)
+    input_data = np.random.rand(1, 784)
+    # Call the memory-intensive prediction function
+    prediction = predict(input_data)
+    b = [2] * (2 * 10 ** 7)
+    del b
+    return {"prediction": prediction.tolist()}
 
 
 if __name__ == "__main__":
